a505572904ef54fe3509713bf8889744040f847e
Nguyễn Văn Tiến <ngtien137@gmail.com>
2021-11-07 13:38:57 +0700

    Change to kotlin 1.5.20

--- app/src/main/java/com/tienuu/demostaggeredlistview/viewmodels/PreviewViewModel.kt
+++ app/src/main/java/com/tienuu/demostaggeredlistview/viewmodels/PreviewViewModel.kt
@@ -2,9 +2,8 @@ package com.tienuu.demostaggeredlistview.viewmodels
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import javax.inject.Inject
 
-class PreviewViewModel @Inject constructor(val stateHandle: SavedStateHandle) : ViewModel() {
+class PreviewViewModel constructor(val stateHandle: SavedStateHandle) : ViewModel() {
 
     val liveImageData = stateHandle.getLiveData<String>("data_image")
 
